2c525c72c815ccf68e2e6273b8935145e669abff
aryatheabhishek <a4abhishek2015@gmail.com>
2022-02-01 21:22:43 +0530

    changes readme file

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# talview
+# talview assignment question B
+
+## Available Scripts
+
+### `npm start`
+
+In the project directory, you can run:
+
+### `npm install`
+
+To install the dependencies in the local node_modules folder
+To install a package and any packages that it depends on
+
+### Request
+
+`POST /api/survey/send_data`
+
+    Sending raw body data in JSON format
+    'Accept: application/json' http://localhost:5000/api/survey/send_data
+
+### Response
+
+    {
+      "message": "From Submitted Successfully"
+    }
+
+### Database
+
+    Using MongoDB
--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
-const axios = require("axios");
-// const cors = require("cors");
 const bodyParser = require("body-parser");
 
 dotenv.config({ path: "./config.env" });
@@ -11,29 +9,10 @@ require("./db/conn");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
-// app.use(cors());
 app.use(express.json());
 
 const survey = require("./controller/survey");
 app.use("/api/survey", survey);
-
-// axios
-//   .get("https://jsonplaceholder.typicode.com/users")
-//   .then((res) => {
-//     const headerDate =
-//       res.headers && res.headers.date ? res.headers.date : "no response date";
-//     console.log("Status Code:", res.status);
-//     console.log("Date in Response header:", headerDate);
-
-//     const users = res.data;
-
-//     for (user of users) {
-//       console.log(`Got user with id: ${user.id}, name: ${user.name}`);
-//     }
-//   })
-//   .catch((err) => {
-//     console.log("Error: ", err.message);
-//   });
 
 const PORT = process.env.PORT;
 
